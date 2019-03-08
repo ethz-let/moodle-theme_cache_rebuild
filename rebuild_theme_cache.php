88ab6e32119af7bf1a8a4d3d7d51286973ff6890
Nexterday <nexterday@users.noreply.github.com>
2019-03-08 11:20:19 +0100

    Update rebuild_theme_cache.php

--- rebuild_theme_cache.php
+++ rebuild_theme_cache.php
@@ -8,15 +8,19 @@ This script should be added to ./admin/cli/ and called via commandline.
 define('CLI_SCRIPT', true);
 require(__DIR__.'/../../config.php');
 require_once($CFG->dirroot.'/lib/csslib.php');
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 $themename = $CFG->theme;
 $theme= theme_config::load($themename);
 $rev = theme_get_revision();
 $candidatedir = "$CFG->localcachedir/theme/$rev/$themename/css";
 $candidatesheet = "$candidatedir/all.css";
 if (file_exists($candidatesheet)) {
+        echo "Not Generated. Theme compiled CSS was already generated: " . $candidatesheet ;
         exit;
 } else {
         $csscontent = $theme->get_css_content();
         make_localcache_directory('theme', false);
         css_store_css($theme, "$candidatedir/all.css", $csscontent);
+        echo "Generated. Theme compiled CSS is generated: " . $candidatesheet;
 }
